994657d6d0d20463ba88732167586929870bc39b
Patryk Świechowski <patryk.s9521@gmail.com>
2023-04-23 19:00:44 +0200

    Add event handler

--- js/script.js
+++ js/script.js
@@ -5,11 +5,13 @@
   });*/
 
 const titleClickHandler = function (event) {
+  event.preventDefault();
   const clickedElement = this;
   console.log(event);
   console.log('Link was clicked!');
 
   /* remove class 'active' from all article links  */
+
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for (let activeLink of activeLinks) {
@@ -17,11 +19,12 @@ const titleClickHandler = function (event) {
   }
 
   /* add class 'active' to the clicked link */
+
   clickedElement.classList.add('active');
   console.log('clickedElement:', clickedElement);
 
   /* remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.posts article.active')
+  const activeArticles = document.querySelectorAll('.posts article.active');
 
   for (let activeArticle of activeArticles) {
     activeArticle.classList.remove('active');
@@ -29,9 +32,17 @@ const titleClickHandler = function (event) {
 
   /* get 'href' attribute from the clicked link */
 
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log('Clicked article' + articleSelector);
+
   /* find the correct article using the selector (value of 'href' attribute) */
 
+  const correctArticle = document.querySelector(articleSelector);
+  console.log(correctArticle);
+
   /* add class 'active' to the correct article */
+  correctArticle.classList.add('active');
+
 }
 
 const links = document.querySelectorAll('.titles a');
