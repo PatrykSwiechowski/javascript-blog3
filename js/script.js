fd6d47c2c66c30afdbf67793d785733f59917810
Patryk Świechowski <patryk.s9521@gmail.com>
2023-05-22 23:47:06 +0200

    js 7

--- js/script.js
+++ js/script.js
@@ -55,24 +55,66 @@ function generateTitleLinks() {
     const articleId = article.getAttribute('id');
     console.log(articleId);
     /* find the title element */ /* get the title from the title element */
-    const articleTitle = article.querySelector(optTitleSelector).innerHTML; //innerHTML wyciąga sam tekst HTML
+    const articleTitle = article.querySelector(optTitleSelector)//innerHTML; //innerHTML wyciąga sam tekst HTML
     console.log(articleTitle);
     /* create HTML of the link */
     const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
     /* insert link into titleList */
     html = html + linkHTML;
-
-
+    
     const links = document.querySelectorAll('.titles a');
     console.log(links);
 
     for (let link of links) {
       link.addEventListener('click', titleClickHandler);
     }
+    titleList.innerHTML;
   }
-  titleList.innerHTML;
 }
 generateTitleLinks();
+
+function generateTags() {
+  /* find all articles */
+  const articles = document.querySelectorAll(optArticleSelector);
+  /* START LOOP: for every article: */
+  for (let article of articles) {
+    /* find tags wrapper */
+    const wrapperTags = article.querySelectorAll(optArticleTagsSelector);
+    console.log(wrapperTags);
+    /* make html variable with empty string */
+    let html = '';
+    /* get tags from data-tags attribute */
+    const articleTags = article.getAttribute('data-tags');
+    console.log(articleTags);
+    /* split tags into array */
+    const articleTagsArray = articleTags.split(' ');
+    console.log(articleTagsArray);
+    /* START LOOP: for each tag */
+    for (let tag of articleTagsArray) {
+      console.log(tag);
+      /* generate HTML of the link */
+      const tagHtml = '<li><a href=#tag-' + tag + ">" + tag + "</a></li >";
+      console.log(tagHtml);
+      /* add generated code to html variable */
+      html = html + tagHtml;
+      console.log(html);
+      /* END LOOP: for each tag */
+    }
+    /* insert HTML of all the links into the tags wrapper */
+    wrapperTags.innerHTML
+    console.log(wrapperTags);
+
+  }
+
+
+
+
+  /* insert HTML of all the links into the tags wrapper */
+
+  /* END LOOP: for every article: */
+}
+
+generateTags();
 
 
 
